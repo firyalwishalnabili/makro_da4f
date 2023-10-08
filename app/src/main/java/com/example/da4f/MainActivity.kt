a61f7a224e3660ad4ef5d607d7e5a7b5dde8cffd
unknown <firyalnabili@gmail.com>
2023-10-09 06:48:08 +0700

    Task-1-Androfun-Firyal Wishal Nabili-A-Aft

--- app/src/main/java/com/example/da4f/MainActivity.kt
+++ app/src/main/java/com/example/da4f/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.da4f
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tv:TextView
+    private lateinit var tvsatu:TextView
+    private lateinit var tvdua:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tvsatu = findViewById(R.id.tvmain)
         tvdua = findViewById(R.id.tvmain2)
 
+        val btnFragment:Button = findViewById(R.id.btn_fragment)
+        btnFragment.setOnClickListener(this)
+
         val username = intent.getParcelableExtra<User>("User")?.username
         val password = intent.getParcelableExtra<User>("User")?.password
         tvsatu.text = "Username : $username"
         tvdua.text = "Password : $username"
+
+        R.id.btn_fragment ->{
+            val intent = Intent(this@MainActivity, TesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
